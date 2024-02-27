726090ada881047e91b4dca69a70b2dfed6e5f0e
YafiAllamJunaedi <yafiallamjun@gmail.com>
2024-02-27 14:18:47 +0700

    add delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,37 @@ import { Products } from "./data/dataProduct";
 import Form from "./components/Form";
 
 const App = () => {
-
   const initialStateData = {
     nama: "",
     deskripsi: "",
-    image: ""
-  }
+    image: "",
+  };
 
-  const [products, setProducts] = useState(Products)
-  const [data, setData] = useState(initialStateData)
-  const {nama, deskripsi, image} = data
-
+  const [products, setProducts] = useState(Products);
+  const [data, setData] = useState(initialStateData);
+  const { nama, deskripsi, image } = data;
   const [addProduct, setAddProduct] = useState(false);
-
 
   function falseAddProduct() {
     setAddProduct(!addProduct);
   }
-  function handleOnchange(e){
+
+
+  function handleOnchange(e) {
     setData({
       ...data,
       id: products.length + 1,
       [e.target.name]: e.target.value,
     });
   }
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     setProducts([...products, data]);
     setData(initialStateData);
+  }
+  function handleDelete(id) {
+    const filteredItem = products.filter((item) => item.id != id);
+    setProducts(filteredItem);
   }
 
   return (
@@ -53,18 +56,26 @@ const App = () => {
               />
             </svg>
             {addProduct == true ? "close form" : "show form"}
-             
           </button>
         </div>
-        {addProduct && <Form name={nama} description={deskripsi} imageURL={image} onChange={handleOnchange} onSubmit={handleSubmit} />}
+        {addProduct && (
+          <Form
+            name={nama}
+            description={deskripsi}
+            imageURL={image}
+            onChange={handleOnchange}
+            onSubmit={handleSubmit}
+          />
+        )}
       </div>
-      <div className="grid grid-cols-4 m-10 gap-5">
+      <div className="lg:grid lg:grid-cols-4 m-10 gap-5 sm:grid sm:grid-cols-2">
         {products.map((product) => (
           <Card
             key={product.id}
             image={product.image}
             title={product.nama}
             description={product.deskripsi}
+            onclick={() => handleDelete(product.id)}
           />
         ))}
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,44 +3,43 @@ import React from "react";
 const Form = ({ name, description, imageURL, onChange, onSubmit }) => {
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <div className="my-2 flex justify-center">
-          <input
-            className="border-2 w-96"
-            type="text"
-            name="nama"
-            id=""
-            placeholder="Name"
-            value={name}
-            onChange={onChange}
-          />
-        </div>
-        <div className="my-2 flex justify-center">
-          <input
-            className="border-2 w-96"
-            type="text"
-            name="deskripsi"
-            id=""
-            placeholder="Description"
-            value={description}
-            onChange={onChange}
-          />
-        </div>
-        <div className="my-2 flex justify-center">
-          <input
-            className="border-2 w-96"
-            type="text"
-            name="image"
-            id=""
-            placeholder="imageURL"
-            value={imageURL}
-            onChange={onChange}
-          />
-        </div>
-        <div className="my-2 flex justify-center">
-          <input className="border-2 w-96" type="submit" name="" id="" />
-        </div>
-      </form>
+      <div className="flex justify-center">
+        <form onSubmit={onSubmit}>
+          <div className="my-2 grid gap-2">
+            <input
+              className="border-2 w-96"
+              type="text"
+              name="nama"
+              id=""
+              placeholder="Name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              className="border-2 w-96"
+              type="text"
+              name="deskripsi"
+              id=""
+              placeholder="Description"
+              value={description}
+              onChange={onChange}
+            />
+            <input
+              className="border-2 w-96"
+              type="text"
+              name="image"
+              id=""
+              placeholder="imageURL"
+              value={imageURL}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="my-2 flex justify-center">
+            <input className="border-2 w-96" type="submit" name="" id="" />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
